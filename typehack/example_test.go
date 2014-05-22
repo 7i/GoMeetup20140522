02fc921cb0ebb9ac913d718b98449dc05c86e6e8
Daniel Liden <7i.github@gmail.com>
2014-05-22 03:09:07 +0200

    added Test code

--- typehack/example_test.go
+++ typehack/example_test.go
@@ -5,13 +5,18 @@ import typehack "."
 
 func ExampleToByteSlice() {
 	s := "Hello, Gophers!"
-	fmt.Println("ToByteSlice:", ToByteSlice(s))
+	fmt.Println("ToByteSlice:", typehack.ToByteSlice(s))
 	fmt.Println("Raw s:", s)
+	// Output:
+	// ToByteSlice: [72 101 108 108 111 44 32 71 111 112 104 101 114 115 33]
+	// Raw s: Hello, Gophers!
 }
-
 
 func ExampleToString() {
 	bs := []byte("Hello, Gophers!")
-	fmt.Println("ToString:", ToString(bs))
+	fmt.Println("ToString:", typehack.ToString(bs))
 	fmt.Println("Raw bs:", bs)
+	// Output:
+	// ToString: Hello, Gophers!
+	// Raw bs: [72 101 108 108 111 44 32 71 111 112 104 101 114 115 33]
 }
---
+++ typehack/typehack_amd64.s
@@ -0,0 +1,32 @@
+//; func ToByteSlice(s string) (bs []byte)
+TEXT ·ToByteSlice(SB),7,$0
+//       s+0(FP) contains pointer to the s data
+//       s+8(FP) contains length of s
+//       bs+16(FP) contains pointer to bs data
+//       bs+24(FP) contains length of bs
+//       bs+32(FP) contains cap of bs
+
+//; Pointer to data in s is moved to bs pointer so that bs now refers to 
+//; the same data.  
+	MOVQ s+0(FP), AX 
+	MOVQ s+8(FP), BX
+	MOVQ AX, bs+16(FP)  
+	MOVQ BX, bs+24(FP)
+	MOVQ BX, bs+32(FP)
+	RET
+
+//; func ToString(bs []byte) (s string)
+TEXT ·ToString(SB),7,$0
+//       bs+0(FP) contains pointer to the bs data
+//       bs+8(FP) contains length of bs
+//       bs+16(FP) contains cap of bs
+//       s+24(FP) contains pointer to s data
+//       s+32(FP) contains length of s
+
+//; Pointer to data in bs is moved to s pointer so that bs now refers to 
+//; the same data.  
+	MOVQ bs+0(FP), AX
+	MOVQ bs+8(FP), BX 
+	MOVQ AX, s+24(FP)  
+	MOVQ BX, s+32(FP)
+	RET
